bff514cce567e2c1542f35cb5152a0455cb72544
Stuckens <kelian2009@hotmail.fr>
2022-03-14 14:13:41 +0100

    Ajout de la page post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Post;
+use App\Repository\PostRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(PostRepository $post_repo): Response
     {
+        $posts =$post_repo->findBy([],['createdAt' => 'desc']);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'posts' => $posts
         ]);
     }
 }
