5e4c0e7722863b1db7d2888db66b4bdf4d33214a
Vincent Sureau <vincentsureau5@gmail.com>
2022-03-15 15:03:40 +0100

    style + refacto

--- src/Form/PostFormType.php
+++ src/Form/PostFormType.php
@@ -6,14 +6,20 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PostFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Votre question'
+            ])
         ;
     }
     public function configureOptions(OptionsResolver $resolver): void
