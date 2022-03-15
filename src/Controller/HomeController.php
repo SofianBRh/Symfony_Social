2bed07071b613b829289b248b1923d2c95d35472
Stuckens <kelian2009@hotmail.fr>
2022-03-15 12:20:35 +0100

    Feature/Modification d'un post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -54,5 +55,29 @@ class HomeController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route ("/home/{id}/delete", name="app_delete")
+     * @param Post $post
+     * @return RedirectResponse
+     */
+
+    #[Route('/home', name: 'app_delete', methods: ['GET', 'POST'])]
+    public function delete(Post $post, EntityManagerInterface $em):RedirectResponse
+    {
+        $em->remove($post);
+        $em->flush();
+
+        return $this->redirectToRoute(route : "app_home");
+    }
+
+    #[Route("/home/{id}/edit", name: 'app_edit', methods: ['GET', 'POST'])]
+    public function edit(Post $post, Request $request, EntityManagerInterface $em) : Response
+    {
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+        return $this->render('post/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 
 }
