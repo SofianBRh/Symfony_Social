bddf976a52c5d6a3ff9558325c5ed5eff13ede43
Vincent Sureau <vincentsureau5@gmail.com>
2022-03-15 15:46:43 +0100

    added ckeditor

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,17 +4,18 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content', TextareaType::class, [
-                "label" => "Commentaire",
+            ->add('content', CKEditorType::class, [
+                'label' => 'Votre réponse',
+                'input_sync' => true
             ])
         ;
     }
