79ead1c5356f85bcb12a735f771bf010de365bed
Stuckens <kelian2009@hotmail.fr>
2022-03-15 13:42:53 +0100

    feature/edit post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,22 +5,14 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Entity\User;
 use App\Form\PostFormType;
-use App\Security\EmailVerifier;
-use App\Form\RegistrationFormType;
 use App\Repository\PostRepository;
-use App\Repository\UserRepository;
-use Symfony\Component\Mime\Address;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Contracts\Translation\TranslatorInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 
 class HomeController extends AbstractController
